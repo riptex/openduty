fceb1b8623044f045608bbac1bf615b7b7fc2adf
riptex <dscassa@scassa.com>
2017-11-26 11:03:41 -0600

    added the other sh*t as per instructions.

--- openduty/settings.py
+++ openduty/settings.py
@@ -8,6 +8,27 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import djcelery
@@ -162,3 +183,5 @@ if 'test' in sys.argv:
         'django.contrib.auth.hashers.MD5PasswordHasher',
         'django.contrib.auth.hashers.SHA1PasswordHasher',
     )
+    
+    
